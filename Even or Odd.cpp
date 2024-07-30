95fecdaec996e5c334f985d51ce9c431a4886463
Sohamjade <157983419+SohamJade@users.noreply.github.com>
2024-07-30 15:36:35 +0530

    Add files via upload

---
+++ Even or Odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    if(a%2 == 0)
+    {
+        cout<<"It is an even number.";
+    }
+    else
+    {
+        cout<<"It is an odd number.";
+    }
+}
---
+++ Greater number.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the three numbers: ";
+    cin>>a>>b>>c;
+
+    if(a>b)
+    {
+        if(a>c)
+        {
+            cout<<"a is greatest.";
+        }
+        else
+        {
+            cout<<"c is greatest.";
+        }
+    }
+    else if(b>c)
+    {
+        cout<<"b is greatest.";
+    }
+    else
+    {
+        cout<<"c is greatest.";
+    }
+}
